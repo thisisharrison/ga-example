f292f0a5a1e3c4961c4594846b02d71e7f8fbc5c
Harrison Lau <harrison.lau@outlook.com>
2021-11-30 18:10:12 +0800

    update ga

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import "./App.css";
 
 function App() {
     const onClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        ga("send", "event", {
-            eventCategory: "Testing Category",
-            eventAction: "click",
-            eventLabel: e.currentTarget.className,
+        // @ts-ignore
+        window.dataLayer.push({
+            event: "gtm.linkClick",
         });
+        console.log("link click");
     };
+
     return (
         <div className="App">
             <header className="App-header">
@@ -18,7 +19,7 @@ function App() {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a className="App-link" href="#" onClick={onClick}>
-                    Click and check GA
+                    Click and <span>check</span> GA
                 </a>
             </header>
         </div>
